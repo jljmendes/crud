9c693d8534f685fd3a951f30a212ce3517e6b469
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-06-28 23:37:51 -0300

    Criando a tabela posts

--- database/migrations/2022_06_29_022814_create_posts_table.php
+++ database/migrations/2022_06_29_022814_create_posts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->text('conteudo');
+            $table->string('autor');
             $table->timestamps();
         });
     }
